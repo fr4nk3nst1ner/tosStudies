44d7dc58596bab92a22292ce88287e9afad3edb8
Jonathan Stines <jonathan.stines@gmail.com>
2025-07-13 15:24:08 -0500

    added atr value in addition to atr percent

--- 21dayATRpercen.ts
+++ 21dayATRpercen.ts
@@ -12,7 +12,7 @@ def length = if isMonthly then 3 else if isWeekly then 4 else 21;
 def atr = Average(TrueRange(high, close, low), length);
 def atrPercent = (atr / close) * 100;
 
-# Plot
+# Plot ATR Percent (optional to also plot raw ATR below)
 plot ATR_Percent = atrPercent;
 ATR_Percent.SetLineWeight(2);
 ATR_Percent.AssignValueColor(
@@ -21,8 +21,15 @@ ATR_Percent.AssignValueColor(
     else Color.GREEN
 );
 
+# Optional: Plot raw ATR (can comment out if not needed)
+plot ATR_Value = atr;
+ATR_Value.SetDefaultColor(Color.LIGHT_GRAY);
+ATR_Value.SetLineWeight(1);
+ATR_Value.SetStyle(Curve.SHORT_DASH);
+
+# Label with both values
 AddLabel(yes, 
-    (length + "-bar ATR %: " + Round(atrPercent, 2) + "%"), 
+    length + "-bar ATR: " + Round(atr, 2) + " | ATR %: " + Round(atrPercent, 2) + "%",
     if atrPercent > 5 then Color.RED
     else if atrPercent > 3 then Color.ORANGE
     else Color.GREEN
